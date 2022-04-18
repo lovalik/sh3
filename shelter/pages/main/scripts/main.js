585c517f191c8a8a4b1fc32cf91da5a88cc62850
Lobyak Valentin <lovalikk@gmail.com>
2022-04-18 21:21:51 +0300

    feat: add folder scripts for main page, add file main.js

---
+++ shelter/pages/main/scripts/main.js
@@ -0,0 +1,15 @@
+function dgd() {
+
+    const buttonDoubleArrow = document.querySelector( ".double-arrow" );
+    
+    function asd() {
+        alert("111");
+    }
+    
+    buttonDoubleArrow.addEventListener( "click", () => {
+       return alert("dfv");
+    } );
+}
+
+
+export default dgd;
